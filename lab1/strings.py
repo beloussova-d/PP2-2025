0c59a7ffff4a2375930ed755f5f9cd61cb4f4a10
Beloussova Darya <beloussova.d@gmail.com>
2025-01-25 20:22:32 +0500

    added more examples

---
+++ lab1/strings.py
@@ -0,0 +1,9 @@
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
