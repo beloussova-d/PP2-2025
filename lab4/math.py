a0c183d5fb0d07ae654343d2380521cffa1ed500
Beloussova Darya <beloussova.d@gmail.com>
2025-02-15 15:35:06 +0500

    upd

---
+++ lab4/math.py
@@ -0,0 +1,20 @@
+import math
+# 1
+# degree=float(input('Input degree: '))
+# print(f"Output radian: {degree*math.pi/180}")
+
+# 2
+# h=float(input('Height: '))
+# b1=float(input('Base, first value: '))
+# b2=float(input('Base, second value: '))
+# print(f"Expected Output: {(b1+b2)/2*h}")
+
+# 3
+# n=int(input('Input number of sides: '))
+# l=float(input('Input the length of a side: '))
+# print(f"The area of the polygon is: {pow(l,2)*n/(4*math.tan(math.pi/n)):.3f}")
+
+# 4
+b=float(input('Length of base: '))
+h=float(input('Height of parallelogram: '))
+print(f"Expected Output: {b*h}")
