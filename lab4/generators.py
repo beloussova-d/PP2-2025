e86ad0b10bfa143083644d452440df89efab4a27
Beloussova Darya <beloussova.d@gmail.com>
2025-02-22 20:16:59 +0500

    upd

--- lab4/generators.py
+++ lab4/generators.py
@@ -15,3 +15,31 @@ n=int(input())
 # print(s[1:-1])
 
 # 3
+# def divis(n):
+#     num=0
+#     while num<=n:
+#         if num%3==0 or num%4==0:
+#             yield num
+#         num+=1
+# list1=list(divis(n))
+# print(list1)
+
+# 4
+# def squares(a,b):
+#     num=a
+#     while num<=b:
+#         yield num**2
+#         num+=1
+# a=n
+# b=int(input())
+# for i in squares(a,b):
+#     print(i)
+
+# 5
+def ntozero(n):
+    num=n
+    while num>=0:
+        yield num
+        num-=1
+for i in ntozero(n):
+    print(i)
